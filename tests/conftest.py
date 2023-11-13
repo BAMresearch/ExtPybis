48ee0a246a0ab8ff1fc2105131f5c6918ef6bdb1
cezary17 <cezarykujath@gmail.com>
2023-11-13 10:30:26 +0100

    changed calling of init docker

--- tests/conftest.py
+++ tests/conftest.py
@@ -18,16 +18,3 @@ def pytest_addoption(parser):
     parser.addoption("--login", action="store", default="no_cl_login")
     parser.addoption("--password", action="store", default="no_cl_password")
     parser.addoption("--url", action="store", default="https://localhost:8443/openbis/")
-
-
-# @pytest.fixture(scope='session', autouse=True)
-# def pytest_generate_tests(metafunc):
-#     # This is called for every test. Only get/set command line arguments
-#     # if the argument is specified in the list of test "fixturenames".
-#     option_value_login = metafunc.config.option.login
-#     if 'login' in metafunc.fixturenames and option_value_login is not None:
-#         metafunc.parametrize("login", [option_value_login])
-#
-#     option_value_password = metafunc.config.option.password
-#     if 'password' in metafunc.fixturenames and option_value_password is not None:
-#         metafunc.parametrize("password", [option_value_password])
--- tests/init_docker/init_settings_docker.py
+++ tests/init_docker/init_settings_docker.py
@@ -3,23 +3,25 @@ from pathlib import Path
 import json
 import argparse
 
-settings_path = Path("./init_settings.json")
+if __name__ == "__main__":
 
-argParser = argparse.ArgumentParser()
-argParser.add_argument("-l", "--login", help="openbis login")
-argParser.add_argument("-p", "--password", help="openbis password")
-argParser.add_argument("-u", "--url", help="openbis url address")
+    settings_path = Path("./init_settings.json")
 
-args = argParser.parse_args()
+    argParser = argparse.ArgumentParser()
+    argParser.add_argument("-l", "--login", help="openbis login")
+    argParser.add_argument("-p", "--password", help="openbis password")
+    argParser.add_argument("-u", "--url", help="openbis url address")
 
-o = Interbis(args.url, verify_certificates=False)
-o.connect_to_datastore(args.login, args.password)
+    args = argParser.parse_args()
 
-with open(settings_path, 'r') as file:
-    default_settings = json.load(file)
+    o = Interbis(args.url, verify_certificates=False)
+    o.connect_to_datastore(args.login, args.password)
 
-settings_sample = o.get_sample("/ELN_SETTINGS/GENERAL_ELN_SETTINGS")
-settings_sample.props["$eln_settings"] = json.dumps(default_settings)
-settings_sample.save()
+    with open(settings_path, 'r') as file:
+        default_settings = json.load(file)
 
-o.logout()
+    settings_sample = o.get_sample("/ELN_SETTINGS/GENERAL_ELN_SETTINGS")
+    settings_sample.props["$eln_settings"] = json.dumps(default_settings)
+    settings_sample.save()
+
+    o.logout()
